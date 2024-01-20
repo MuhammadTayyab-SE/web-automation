a81347c209a309cc44f34e7fd8068d45b6fff19a
muhammad.tayyab01 <muhammadtayyab.se@gmail.com>
2024-01-21 01:16:06 +0500

    scraping dynamic value from the browser

---
+++ scraping_dynamic_text.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import time
+
+def get_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument('disable-infobars') #this will datable info bars.
+    options.add_argument('start-maximized') #start browsers with maximized window size
+    options.add_argument('disable-dev-shm-usage') #disable particular issues with browser on linux computers.
+    options.add_argument('no-sandbox')
+    options.add_experimental_option('excludeSwitches',['enable-automation'])
+    options.add_argument("disable-blink-features=AutomationControlled")
+    
+    driver = webdriver.Chrome(options=options)
+    web_page_url = "https://automated.pythonanywhere.com/"
+    driver.get(web_page_url)
+    return driver
+def main():
+    driver = get_driver()
+    
+    # adding sleeper on the page
+    time.sleep(2)
+    element = driver.find_element(by = 'xpath', value="/html/body/div[1]/div/h1[2]")
+
+    return element.text
+print(main())
